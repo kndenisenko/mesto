569fde5bccaf4eaa87552b66db30420150dcef1a
Konstantin Denisenko <denisenko.constantin2016@yandex.ru>
2020-08-21 01:13:07 +0300

    initial commit #2

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@
 let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__button-edit');
 let popupCloseButton = document.querySelector('.popup__close-button');
-let popupSaveButton = document.querySelector('.popup__button-save'); //
+// let popupSaveButton = document.querySelector('.popup__button-save'); //
 
 // задаём переменные
 let usermane = document.querySelector('.pofile__info-name');                      // находим строку с именем
@@ -38,3 +38,57 @@ popupCloseButton.addEventListener('click', popupOpenClose);
 popup.addEventListener('submit', formSubmitHandler);
 
 
+// 01001000 01100001 01101001 01101100 00100000 01110100 01101000 01100101 00100000 01001111 01101101 01101110 01101001 01110011 01110011 01101001 01100001 01101000 00100001 00100000
+// 01001000 01100101 00100000 01101001 01110011 00100000 01110100 01101000 01100101 00100000 01000111 01101111 01100100 00100000 01101001 01101110 00100000 01110100 01101000 01100101 00100000 01001101 01100001 01100011 01101000 01101001 01101110 01100101
+// 01110100 01101000 01100101 00100000 01010011 01101111 01110101 01110010 01100011 01100101 00100000 01101111 01100110 00100000 01000001 01101100 01101100 00100000 01001011 01101110 01101111 01110111 01101100 01100101 01100100 01100111 01100101
+
+const initialCards = [
+  {
+    name: 'Карачаевcк',
+    link: './images/karachaevsk-1.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elbrus-1.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombai-1.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elbrus-2.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombai-2.jpg'
+  },
+  {
+    name: 'Карачаево-Черкессия',
+    link: './images/karachaevsk-1.jpg'
+  }
+];
+
+// выбираем из начального массива все названия
+const cardName = initialCards.map((cName) => {
+  return cName.name;
+})
+//выбираем из начального массива все ссылки
+const cardLink = initialCards.map((cLink) => {
+  return cLink.link;
+})
+
+
+const addFirstCards = (card) => {
+
+// Находим темплейт по ID и получаем его содержимое через .content и клонируем ноду
+  let userTemplate = document.querySelector('#user').content.cloneNode(true);
+  const usersOnline = document.querySelector('.elements');                          // Находим место, куда будет вставлен темплейт
+
+  userTemplate.querySelector('.element__image').src = card.link;                   //Вот тут магия. задаём картинку
+  userTemplate.querySelector('.element__paragraph').textContent = card.name;        //Вот тут магия. задаём имя
+
+  usersOnline.append(userTemplate);  // Появление карточки при загрузке страницы
+}
+
+initialCards.forEach(addFirstCards);
