38186b30482faa0e12fb1cbe98aa1d5800aa75be
Konstantin Denisenko <denisenko.constantin2016@yandex.ru>
2020-08-21 17:08:56 +0300

    Develop: функция открытия попапа берёт названия из JS. Изменены ID в попапе

--- scripts/script.js
+++ scripts/script.js
@@ -10,15 +10,19 @@ let popupCloseButton = document.querySelector('.popup__close-button');
 let usermane = document.querySelector('.pofile__info-name');                      // находим строку с именем
 let occupation = document.querySelector('.pofile__info-occupation');              // находим строку с профессией
 
-// функция отвечает за запуск формы
+// функция отвечает за открытие/закрытие попапа и текст внутри него
 function popupOpenClose() {
-  let formFieldFirst = document.getElementById('username');                       // находим первую форму в попапе
-  formFieldFirst.setAttribute('value', usermane.textContent);                     // заменяем значение инпута
+  let formFieldFirst = document.getElementById('firstInput');                           // находим первую форму в попапе
+  formFieldFirst.setAttribute('value', usermane.textContent);                       // заменяем значение инпута
+  document.querySelector('.popup__paragraph').textContent = 'Редактировать профиль';    // добавляем заголовок
+  document.querySelector('.popup__button-save').textContent = 'Сохранить';
 
-  let formFieldSecond = document.getElementById('occupation');                    // Повторяем всё тоже самое, для заполения второй формы
+  let formFieldSecond = document.getElementById('secondInput');                         // Повторяем всё тоже самое, для
+  // заполения второй формы
   formFieldSecond.setAttribute('value', occupation.textContent);
 
-  popup.classList.toggle('popup_toggle');                                         // открываем/закрываем попап по кнопке Х
+  popup.classList.toggle('popup_toggle');                                                  // открываем/закрываем
+  // попап по кнопке Х
 }
 
 // Функция отвечает за "сохранение" имени и профессии
@@ -45,50 +49,60 @@ popup.addEventListener('submit', formSubmitHandler);
 const initialCards = [
   {
     name: 'Карачаевcк',
-    link: './images/karachaevsk-1.jpg'
+    link: './images/karachaevsk-1.jpg',
+    alt: 'Гора на Кавказе'
   },
   {
     name: 'Гора Эльбрус',
-    link: './images/elbrus-1.jpg'
+    link: './images/elbrus-1.jpg',
+    alt: 'Гора Эльбрус'
   },
   {
     name: 'Домбай',
-    link: './images/dombai-1.jpg'
+    link: './images/dombai-1.jpg',
+    alt: 'Лыжный курорт на Домбае'
   },
   {
     name: 'Гора Эльбрус',
-    link: './images/elbrus-2.jpg'
+    link: './images/elbrus-2.jpg',
+    alt: 'Гора Эльбрус'
   },
   {
     name: 'Домбай',
-    link: './images/dombai-2.jpg'
+    link: './images/dombai-2.jpg',
+    alt: 'Домбай летом'
   },
   {
     name: 'Карачаево-Черкессия',
-    link: './images/karachaevsk-1.jpg'
+    link: './images/karachaevsk-1.jpg',
+    alt: 'Гора на Кавказе'
   }
 ];
 
-// выбираем из начального массива все названия
-const cardName = initialCards.map((cName) => {
-  return cName.name;
-})
-//выбираем из начального массива все ссылки
-const cardLink = initialCards.map((cLink) => {
-  return cLink.link;
-})
-
-
+// Вывод первых карточек
 const addFirstCards = (card) => {
-
 // Находим темплейт по ID и получаем его содержимое через .content и клонируем ноду
   let userTemplate = document.querySelector('#user').content.cloneNode(true);
-  const usersOnline = document.querySelector('.elements');                          // Находим место, куда будет вставлен темплейт
+  const cardContent = document.querySelector('.elements');                           // Находим место, куда будет
+  // вставлен темплейт
 
-  userTemplate.querySelector('.element__image').src = card.link;                   //Вот тут магия. задаём картинку
-  userTemplate.querySelector('.element__paragraph').textContent = card.name;        //Вот тут магия. задаём имя
+  userTemplate.querySelector('.element__image').src = card.link;                     //Вот тут магия. задаём картинку
+  userTemplate.querySelector('.element__image').alt = card.alt;                      //И снова магия. задаём alt
+  userTemplate.querySelector('.element__paragraph').textContent = card.name;        //Вжух! Имя.
 
-  usersOnline.append(userTemplate);  // Появление карточки при загрузке страницы
+  cardContent.append(userTemplate);  // Появление карточки при загрузке страницы
 }
-
 initialCards.forEach(addFirstCards);
+
+
+
+
+
+
+
+
+
+
+
+
+
