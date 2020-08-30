53d7d0fd00a5aff2c08d87a498eb0d62f48df089
Konstantin Denisenko <denisenko.constantin2016@yandex.ru>
2020-08-31 00:12:39 +0300

    hotfixes: Переменные переделаны в константы и вынесены из функции

--- scripts/script.js
+++ scripts/script.js
@@ -82,14 +82,10 @@ const initialCards = [
 // Находим темплейт по ID и получаем его содержимое через .content и находим место для клонирования
 const userTemplate = document.querySelector('#card').content;                // Находим темплейт
 const cloneTarget = document.querySelector('.elements');                     // Находим место куда будем клонировать
-                                    // клонируем
-// function ret() {
-//   let abc = 50
-//   return abc;
-// }
-// let zzz = ret();
-// console.log(zzz)
-//
+
+// переменные для записи имени карточки, ссылки на фото и alt
+let newCardimg = document.querySelector('.popup__photobox-image');
+let newCardtext = document.querySelector('.popup__photobox-caption');
 
 //Функция создания карточки
 function addFirstCards (card) {
@@ -116,8 +112,8 @@ function addFirstCards (card) {
   cardNode.querySelector('.element__image').addEventListener('click', event => {
 
     //Вытаскиваем ссылку на изображение и выводим её в консоль, для теста работы функции. Эта функция работает.
-    document.querySelector('.popup__photobox-image').src = event.target.src;
-    document.querySelector('.popup__photobox-caption').textContent = card.name;
+    newCardimg.src = event.target.src;
+    newCardtext.textContent = card.name;
 
     // открытие и закрытие попапа
     imagePopop.classList.toggle('popup_toggle');
@@ -149,16 +145,13 @@ function addNewCardPopup () {
 function createdObj (evt) {
   evt.preventDefault();  //отмена перезагрузки страницы при отправке формы
 
-  // создаём пустой объект
-  let newObject = {
-    name: '',
-    link: '',
-    alt: ''
+  // создаём пустой объект c нужными значениями
+  const newObject = {
+    name: newCardName.value,
+    link: newCardLink.value,
+    alt: newCardName.value
   };
 
-  newObject.name = newCardName.value;
-  newObject.alt = newCardName.value;
-  newObject.link = newCardLink.value;
 
   //закрыть попап
   addCardPopup.classList.toggle('popup_toggle');
