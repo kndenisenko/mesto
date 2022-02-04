a9d824c141f4cbb812786729507c17c5315e6d0f
Konstantin Denisenko <denisenko.constantin2016@yandex.ru>
2022-02-04 09:28:04 +0300

    fixes: fixed JS code

--- scripts/index.js
+++ scripts/index.js
@@ -2,35 +2,46 @@
 // ! 01010000 01110010 01100001 01101001 01110011 01100101 00100000 01110100 01101000 01100101 00100000 01001111 01101101 01101110 01101001 01110011 01110011 01101001 01100001 01101000 00100001
 
 // note попап, кнокпи его открытия и закрытия
-let popup = document.querySelector('.popup');
-let popuOpenButton = document.querySelector('.profile__edit');
-let popupResetButton = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
+const popuOpenButton = document.querySelector('.profile__edit');
+const popupResetButton = document.querySelector('.popup__close-button');
+const popupSaveButton = document.querySelector('.popup__submit-button');
+
 
 // note поиск имени и профессии в HTML-разметке
-let username = document.querySelector('.profile__name');
-let occupation = document.querySelector('.profile__occupation');
+const username = document.querySelector('.profile__name');
+const occupation = document.querySelector('.profile__occupation');
 
 // note поиск полей имени и профессии в попапе
-let nameField = document.getElementById('username');
-let occupationField = document.getElementById('occupation');
+const nameField = document.getElementById('username');
+const occupationField = document.getElementById('occupation');
 
-// note функция открытия и закрытия попапа, без сохранения изменний
+// note функция открытия попапа b добавления данных в поля попапа
 function popupOpen() {
-  popup.classList.toggle('popup__visible');
-  nameField.setAttribute('value', username.textContent);
-  occupationField.setAttribute('value', occupation.textContent);
+  popup.classList.add('popup__visible');
+  nameField.value = username.textContent;
+  occupationField.value = occupation.textContent;
+}
+
+// note функция закрытия попапа
+function popupClose() {
+  popup.classList.remove('popup__visible')
 }
 
 // note сохранение данных из попапа
 function popupCloseAndSave(evt) {
   evt.preventDefault();
-  let newData = document.querySelectorAll('input');
-  username.textContent = newData[0].value;
-  occupation.textContent = newData[1].value;
-  popup.classList.toggle('popup__visible');
+  let newUserName = document.getElementById('username');
+  let newOccupation = document.getElementById('occupation');
+  username.textContent = newUserName.value;
+  occupation.textContent = newOccupation.value;
+  popupClose();
 }
 
 // note обработчики нажатий
 popuOpenButton.addEventListener('click', popupOpen);
-popupResetButton.addEventListener('click', popupOpen);
-popup.addEventListener('submit', popupCloseAndSave);
+popupResetButton.addEventListener('click', popupClose);
+popupSaveButton.addEventListener('click', popupCloseAndSave);
+
+
+
